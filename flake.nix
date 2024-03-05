10b684209876f4f0d6ed720232b59a74c9c70d18
Rafael Girão <git@rafael.ovh>
2024-03-05 17:17:30 +0000

    fix: Replace flake.nix with flake.parts

--- flake.nix
+++ flake.nix
@@ -1,48 +1,94 @@
 {
   description = "Build Android (AOSP) using Nix";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "flake:nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     androidPkgs.url = "github:tadfisher/android-nixpkgs/stable";
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+        inputs.pre-commit-hooks-nix.flakeModule
+        # inputs.devenv.flakeModule
+        inputs.treefmt-nix.flakeModule
 
-  outputs = { self, nixpkgs, androidPkgs, ... }@inputs: let
-    pkgs = import ./pkgs/default.nix { inherit inputs; };
-  in {
-    # robotnixSystem evaluates a robotnix configuration
-    lib.robotnixSystem = configuration: import ./default.nix {
-      inherit configuration pkgs;
-    };
-
-    defaultTemplate = {
-      path = ./template;
-      description = "A basic robotnix configuration";
-    };
-
-    nixosModule = import ./nixos; # Contains all robotnix nixos modules
-    nixosModules.attestation-server = import ./nixos/attestation-server/module.nix;
-
-    packages.x86_64-linux = {
-      manual = (import ./docs { inherit pkgs; }).manual;
-    };
-
-    devShell.x86_64-linux = pkgs.mkShell {
-      name = "robotnix-scripts";
-      nativeBuildInputs = with pkgs; [
-        # For android updater scripts
-        (python3.withPackages (p: with p; [ mypy flake8 pytest ]))
-        gitRepo nix-prefetch-git
-        curl pup jq
-        shellcheck
-        wget
-
-        # For chromium updater script
-        python2 cipd git
-
-        cachix
       ];
-      PYTHONPATH=./scripts;
+      flake = {
+        # Put your original flake attributes here.
+      };
+      #systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        # systems for which you want to build the `perSystem` attributes
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      # perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, pkgs, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        packages = {
+
+          inherit ((import ./docs { inherit pkgs; })) manual;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "robotnix-scripts";
+          nativeBuildInputs = with pkgs; [
+            # For android updater scripts
+            (python3.withPackages (p: with p; [ mypy flake8 pytest ]))
+            gitRepo
+            nix-prefetch-git
+            curl
+            pup
+            jq
+            shellcheck
+            wget
+
+            # For chromium updater script
+            # python2 cipd git # -python2 is EOL.
+
+            cachix
+          ];
+          PYTHONPATH = ./scripts;
+          shellHook = ''
+            # export DEBUG=1
+            ${config.pre-commit.installationScript}
+          '';
+        };
+        pre-commit = {
+          check.enable = true;
+          settings.settings = {
+            deadnix.edit = true;
+          };
+          settings.hooks = {
+            actionlint.enable = true;
+            treefmt.enable = true;
+          };
+        };
+        treefmt.projectRootFile = ./flake.nix;
+        treefmt.programs = {
+          nixpkgs-fmt.enable = true;
+          yamlfmt.enable = true;
+          shfmt.enable = true;
+          mdformat.enable = true;
+          statix.enable = true;
+          deadnix.enable = true;
+        };
+      };
     };
-  };
 }
